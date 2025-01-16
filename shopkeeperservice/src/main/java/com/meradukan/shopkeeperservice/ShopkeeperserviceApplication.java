da3b824b55aa2f1349816e60602f2e7ba1647930
Shailesh Choudhary <125347306+shailProJect@users.noreply.github.com>
2025-01-16 20:48:04 +0530

    Add files via upload

---
+++ shopkeeperservice/src/main/java/com/meradukan/shopkeeperservice/ShopkeeperserviceApplication.java
@@ -0,0 +1,13 @@
+package com.meradukan.shopkeeperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkeeperserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkeeperserviceApplication.class, args);
+	}
+
+}
---
+++ shopkeeperservice/src/test/java/com/meradukan/shopkeeperservice/ShopkeeperserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meradukan.shopkeeperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkeeperserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

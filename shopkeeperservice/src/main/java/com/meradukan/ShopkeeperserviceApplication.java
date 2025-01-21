3b5ef1fd0242b42bf0e0f70a10df71a423af6a33
Shailesh <Shailesh@LAPTOP-O4I6H1F3>
2025-01-21 23:14:26 +0530

    na

--- shopkeeperservice/src/main/java/com/meradukan/ShopkeeperserviceApplication.java
+++ shopkeeperservice/src/main/java/com/meradukan/ShopkeeperserviceApplication.java
@@ -1,9 +1,12 @@
-package com.meradukan.shopkeeperservice;
+package com.meradukan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShopkeeperserviceApplication {
 
 	public static void main(String[] args) {
